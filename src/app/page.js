8bd29b3487a99be1a4925ddeaada1fb0beef11fa
spydyweb <ankitkumar65438@gmail.com>
2023-07-27 11:20:33 +0530

    this is testing

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
   const router=useRouter();
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    <h1>Home page</h1>
+    <h1>Home access page</h1>
     <Link href="/login">Goto Login</Link>
     <button onClick={()=>router.push("/login")}>button Login</button>
 
